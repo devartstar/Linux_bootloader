1126fc38fb7fd790e6e0904d5fd55ddaca91ba22
Devjit Choudhury <devchoudhury@microsoft.com>
2025-06-23 18:42:25 +0530

    chs to lba conversion and checking + format files

--- Project1_MBR_Inspector/mbr_inspect_v4/README.md
+++ Project1_MBR_Inspector/mbr_inspect_v4/README.md
@@ -1,3 +1,6 @@
 ## CHS <> LBA Match
 
 Ensures that the CHS (Cylinder-Head-Sector) and LBA (Logical Block Address) values in each partition entry point to the same location — a common source of disk corruption and BIOS/bootloader issues.
+
+Convert CHS to LBA using BIOS assumptions.
+LBA = (cylinder * heads_per_cylinder + head) * sectors_per_track + (sector - 1);
--- Project1_MBR_Inspector/mbr_inspect_v4/mbr.h
+++ Project1_MBR_Inspector/mbr_inspect_v4/mbr.h
@@ -1,69 +1,70 @@
 /**
-* Header Guard 
-* Ensure file included only once during compilation.
-*/
+ * Header Guard
+ * Ensure file included only once during compilation.
+ */
 #ifndef MBR_H
 #define MBR_H
 
 #include <stdint.h>
 
 /**
-* C compiler uses padding for performance
-* Tell the compiler to align all fields to 1 byte boundaries
-* MBR - byte aligned on disk - so we need to match exact layout
-*/
+ * C compiler uses padding for performance
+ * Tell the compiler to align all fields to 1 byte boundaries
+ * MBR - byte aligned on disk - so we need to match exact layout
+ */
 #pragma pack(1)
 
 typedef struct {
 
-	// 1 Byte - 0x80(bootable) | 0x00(not bootable)
-	uint8_t boot_indicator;
+  // 1 Byte - 0x80(bootable) | 0x00(not bootable)
+  uint8_t boot_indicator;
 
-	// Cylinder header sector of partition start
-	uint8_t start_chs[3];
+  // Cylinder header sector of partition start
+  uint8_t start_chs[3];
 
-	// FileSystem ID - 0x83(Linux) | 0x07(NTFS)
-	uint8_t partition_type;
+  // FileSystem ID - 0x83(Linux) | 0x07(NTFS)
+  uint8_t partition_type;
 
-	// Cylinder header sector of partition end
-	uint8_t end_chs[3];
+  // Cylinder header sector of partition end
+  uint8_t end_chs[3];
 
-	// Start sector (Logical Block Addressing)
-	uint32_t start_lba;
+  // Start sector (Logical Block Addressing)
+  uint32_t start_lba;
 
-	// Number of sectors in partition snaps
-	uint32_t num_sectors;
+  // Number of sectors in partition snaps
+  uint32_t num_sectors;
 
 } PartitionEntry; // 16B
 
 typedef struct {
 
-	// BIOS Bootloader code = 446B
-	uint8_t bootloader[446];
+  // BIOS Bootloader code = 446B
+  uint8_t bootloader[446];
 
-	// Partition Entries = 16 * 4 B
-	PartitionEntry partitions[4];
+  // Partition Entries = 16 * 4 B
+  PartitionEntry partitions[4];
 
-	// Boot Signature - Magic Number 0x55AA for BIOS to be bootable
-	uint16_t boot_signature;
+  // Boot Signature - Magic Number 0x55AA for BIOS to be bootable
+  uint16_t boot_signature;
 
-} MBR;	// 512B
+} MBR; // 512B
 
 // End 1 byte alignment directive
 #pragma pack(pop)
 
 // parses the raw 512B into MBR struct
-int parse_mbr(const char* filepath, MBR* out_mbr);
+int parse_mbr(const char *filepath, MBR *out_mbr);
 
 // decode and print the chs for each partition
-void decode_chs(const uint8_t chs[3], int* cylinder, int* head, int* sector);
+void decode_chs(const uint8_t chs[3], int *cylinder, int *head, int *sector);
 
 // dump the raw value of mbr in hex format
-void dump_mbr_raw(const MBR* mbr);
-void dump_mbr_raw_distinguished(const MBR* mbr);
+void dump_mbr_raw(const MBR *mbr);
+void dump_mbr_raw_distinguished(const MBR *mbr);
+
+uint32_t convert_chs_to_lba(int cylinder, int head, int sector);
 
 // print the MBR info
-void print_mbr_info(const MBR* mbr);
+void print_mbr_info(const MBR *mbr);
 
 #endif
-
