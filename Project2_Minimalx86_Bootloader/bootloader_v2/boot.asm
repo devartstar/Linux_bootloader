fdb06f7fec45adbd91e8a0b5fef448cffe223c43
Devjit Choudhury <devchoudhury@microsoft.com>
2025-06-25 21:22:02 +0530

    Making it work

--- Project2_Minimalx86_Bootloader/bootloader_v2/boot.asm
+++ Project2_Minimalx86_Bootloader/bootloader_v2/boot.asm
@@ -1,92 +1,67 @@
 [BITS 16]
-
-; [ORG 0x7C00]
+[ORG 0x7C00]
 
 start:
-	; disable interrupts
-	; Clearning Interrupt flag in Flags Register
-	; CPU ignores all maskable interupts
-	cli	
+    cli                 ; Disable interrupts
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    sti                 ; Enable interrupts
 
-	xor ax, ax
-	mov ds, ax
-	mov es, ax
-	
-	; enables Hardware interrupts
-	sti
+    ; Print hello message from boot sector
+    mov si, hello_msg
+    call print_str
 
-	; debugging
-	mov si, hello_msg
-	call print_str
+    ; Load 1 sector from CHS=(0,0,2) into ES:BX = 0x0500:0x0000 => physical 0x05000
+    mov ax, 0x0500
+    mov es, ax
+    mov bx, 0x0000
+    mov ah, 0x02        ; BIOS: read sectors
+    mov al, 1           ; number of sectors
+    mov ch, 0           ; cylinder
+    mov cl, 2           ; sector (starts from 1)
+    mov dh, 0           ; head
+    mov dl, 0x80        ; first hard disk
+    int 0x13
+    jc disk_error       ; if carry set, read failed
 
-	; BIOS Reads and load 1 sector at CHS = (0,0,2) into Memory 0x0500
-	mov ax, 0x0500
-	mov es, ax
-	mov bx, 0x0000
-	mov ah, 0x02
-	mov al, 1
-	mov ch, 0
-	mov cl, 2
-	mov dh, 0
-	mov dl, 0x80
-	mov bx, 0x0500
-	mov es, ax
-	int 0x13
-
-	; if read failed
-	jc disk_error
-
-	; print the 16 bytes in the sector at (0, 0, 2)
-	mov ax, 0
-	mov ds, ax
-	mov si, 0x5000
-	mov cx, 16
+    ; Print the 16 bytes from the loaded sector
+    mov ax, 0x0000
+    mov ds, ax
+    mov si, 0x5000
+    mov cx, 16
 
 print_loop:
-	lodsb
-	cmp al, 0
-	je halt
-	mov ah, 0x0E
-	int 0x10
-	loop print_loop
+    lodsb
+    cmp al, 0
+    je halt
+    mov ah, 0x0E
+    int 0x10
+    loop print_loop
 
 halt:
-	jmp $
+    jmp $
 
 disk_error:
-	mov si, err_msg
-	call print_str
-	jmp $
+    mov si, err_msg
+    call print_str
+    jmp $
 
 print_str:
-	; loads byte form si to al
-	; then increments si++
-	lodsb
-
-	; if loaded byte = 0
-	cmp al,0
-	je .done
-
-	; points to the BIOS TTY teletype output function
-	mov ah, 0x0E
-
-	; calss BIOS video interrupt
-	; prints the character in the screen and advances cursor
-	int 0x10
-
-	; loop
-	jmp print_str
-
+    lodsb
+    cmp al, 0
+    je .done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_str
 .done:
-	ret
+    ret
 
 hello_msg:
-	db "Hello Boot", 0
+    db "Hello Boot", 0
 
 err_msg:
-	db "Disk Read Error!", 0
+    db "Disk Read Error!", 0
 
-; Pad boot sector
-times 510-($-$$) db 0
+times 510 - ($ - $$) db 0
 dw 0xAA55
-
