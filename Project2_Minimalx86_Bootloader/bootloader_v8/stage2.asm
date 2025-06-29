8de0607296d4d24226decb9f4956fde2f1df2111
devartstar <devjit.choudhury3@gmail.com>
2025-06-29 10:50:33 +0000

    Fix stage2 code to compile

--- Project2_Minimalx86_Bootloader/bootloader_v8/stage2.asm
+++ Project2_Minimalx86_Bootloader/bootloader_v8/stage2.asm
@@ -21,6 +21,79 @@ start:
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;		 INPUT HANDELLING		;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.read_loop:
+	xor ax,ax
+	; 0x16 is BIOS keyboard service
+	int 0x16
+	; ah = 0, it waits for key press
+	; value stored in al
+
+	cmp al, 0x0D	; if <ENTER> is pressed
+	je .done_input
+
+	cmp al, 0x08	; if <BACKSPACE> is pressed
+	je .handle_backspace
+
+	; Regular character is pressed
+	mov [di], al
+	inc di
+	inc cx
+
+	; Print the character pressed in input line
+	mov ah, 0x0E
+	int 0x10
+	jmp .read_loop
+
+.handle_backspace:
+	cmp cx, 0	; if nothing is typed
+	je .read_loop	; ignore and continue reading
+
+	; di points to the current input buffer location
+	dec di		; move a step back in inp. buffer location	
+	dec cx		; decrement number of characters
+
+	; TRICK: FOR HANDELLING BACKSPACE
+	; 1. Move the cursor back
+	mov ah, 0x0E
+	mov al, 0x08	; 0x08 is for backspace
+	int 0x10
+	; 2. Print Space
+	mov al, ' '
+	int 0x10
+	; 3. Move the cursor back again
+	mov al, 0x08
+	int 0x10
+	jmp .read_loop
+
+.done_input:
+
+	; Input is "name"
+	mov si, input_buf
+	call .str_compare_name
+	je .cmd_name
+
+	; Input is "help"
+	mov si, input_buf
+	call .str_compare_help
+	je .cmd_help		; carry falg = 0, ie. success operation
+	
+	; Input is "clear"
+	mov si, input_buf
+	call .str_compare_clear
+	je .cmd_clear
+
+	; Default message for invalid input
+	mov si, msg_help
+	call print_str
+	jmp .reset
+
+.reset:
+	jmp start
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;		STRING COMPARISION		;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; String Comaprision
@@ -88,13 +161,6 @@ start:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;		PRINT HANDELLING		;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-print_str:
-	lodsb
-	cmp al, 0
-	je .done
-	mov ah, 0x0E
-	int 0x10
-	jmp print_str
 
 ; Print ax in hex format
 print_hex16:
@@ -104,6 +170,14 @@ print_hex16:
 	push dx
 
 	mov cx, 4
+
+print_str:
+	lodsb
+	cmp al, 0
+	je .done
+	mov ah, 0x0E
+	int 0x10
+	jmp print_str
 
 .next_nibble:
 
@@ -133,80 +207,7 @@ print_hex16:
 	ret
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;		 INPUT HANDELLING		;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-.read_loop:
-	xor ax,ax
-	; 0x16 is BIOS keyboard service
-	int 0x16
-	; ah = 0, it waits for key press
-	; value stored in al
-
-	cmp al, 0x0D	; if <ENTER> is pressed
-	je .done_input
-
-	cmp al, 0x08	; if <BACKSPACE> is pressed
-	je .handle_backspace
-
-	; Regular character is pressed
-	mov [di], al
-	inc di
-	inc cx
-
-	; Print the character pressed in input line
-	mov ah, 0x0E
-	int 0x10
-	jmp .read_loop
-
-.handle_backspace:
-	cmp cx, 0	; if nothing is typed
-	je .read_loop	; ignore and continue reading
-
-	; di points to the current input buffer location
-	dec di		; move a step back in inp. buffer location	
-	dec cx		; decrement number of characters
-
-	; TRICK: FOR HANDELLING BACKSPACE
-	; 1. Move the cursor back
-	mov ah, 0x0E
-	mov al, 0x08	; 0x08 is for backspace
-	int 0x10
-	; 2. Print Space
-	mov al, ' '
-	int 0x10
-	; 3. Move the cursor back again
-	mov al, 0x08
-	int 0x10
-	jmp .read_loop
-
-.done_input:
-
-	; Input is "name"
-	mov si, input_buf
-	call str_compare_name
-	je .cmd_name
-
-	; Input is "help"
-	mov si, input_buf
-	call str_compare_help
-	je .cmd_help		; carry falg = 0, ie. success operation
-	
-	; Input is "clear"
-	mov si, input_buf
-	call str_compare_clear
-	je .cmd_clear
-
-	; Default message for invalid input
-	mov si, msg_help
-	call print_str
-	jmp .reset
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-.reset:
-	jmp start
-
 .done:
 	ret
 
