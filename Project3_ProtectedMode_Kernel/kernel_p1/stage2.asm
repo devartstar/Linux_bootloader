67807ae6ac825a352dfb06f6decc34be22aaece7
Devjit Choudhury <devchoudhury@microsoft.com>
2025-06-30 22:11:40 +0530

    still debugging...

--- Project3_ProtectedMode_Kernel/kernel_p1/stage2.asm
+++ Project3_ProtectedMode_Kernel/kernel_p1/stage2.asm
@@ -1,44 +1,46 @@
 [BITS 16]
-[ORG 0x0600]  ; FIX 1: Uncomment this line
-; stage2 bootloader will be loaded at 0x0600 mem loc by stage1
+[ORG 0x0000]  ; We are loaded at 0x0600:0000, so offset should be 0
+
+; This file is loaded by Stage1 into 0x0600:0000
 
 start:
+    ; Print single debug character
     mov ah, 0x0E
     mov al, 'x'
     int 0x10
 
+    ; Set up data segments
     cli
     xor ax, ax
     mov ds, ax
     mov es, ax
     sti
 
-    ; Debug Print to check if we reach here
+    ; Debug message: Stage 2 active
     mov si, msg
     call print_str
 
-    ; Load 2 sectors from LBA=4 CHS=(0,0,5) into 0x1000:0000(segment:offset)
-    mov ax, 0x1000	; segment = 0x1000
+    ; Load 2 sectors from CHS = (0,0,5) → LBA = 4
+    ; Destination = 0x1000:0000
+    mov ax, 0x1000        ; Segment 0x1000
     mov es, ax
-    xor bx, bx	; offset = 0
+    xor bx, bx            ; Offset 0
 
-    mov ah, 0x02	; sets up the function (0x02) 
-            ; to be called on BIOS interrupt
-    ; 0x02 means read sectors form the disk
-    ; set up the location for read
-    mov al, 2	; read 2 sectors
-    mov ch, 0	; cylinder
-    mov cl, 5	; sector = 5 (LBA=4 (0,0,5))
-    mov dh, 0
-    mov dl, 0x80	; Read deom Hard Disk
+    mov ah, 0x02          ; BIOS function: read sectors
+    mov al, 2             ; Number of sectors = 2
+    mov ch, 0             ; Cylinder = 0
+    mov cl, 5             ; Sector = 5 (LBA = 4)
+    mov dh, 0             ; Head = 0
+    mov dl, 0x80          ; First hard disk
 
-    int 0x13	; BIOS interrupt for read
-    jc disk_error	; Error reading disk
+    int 0x13              ; Call BIOS disk service
+    jc disk_error         ; If error, jump to error handler
 
-    ; Debug Print to confirm contents loaded from disk
+    ; Confirm kernel loaded
     mov si, loaded_msg
     call print_str
 
+    ; Stop execution for now
     jmp $
 
 disk_error:
@@ -46,27 +48,26 @@ disk_error:
     call print_str
     jmp $
 
+; Subroutine: Print null-terminated string at DS:SI
 print_str:
-    push ax              ; Save registers
+    push ax
     push si
 .loop:
-    lodsb               ; Load byte from [DS:SI] into AL
-    cmp al, 0           ; Check for null terminator
+    lodsb
+    cmp al, 0
     je .done
-    mov ah, 0x0E        ; Make sure AH is set for each character
-    int 0x10            ; Print character
+    mov ah, 0x0E
+    int 0x10
     jmp .loop
 .done:
-    pop si              ; Restore registers
+    pop si
     pop ax
     ret
 
-; FIX 2: Remove this duplicate .done label
-; .done:
-;	ret
+; Messages
+msg:         db "[Stage2 Bootloader] Loading Kernel...", 0x0D, 0x0A, 0
+loaded_msg:  db "[Stage2 Bootloader] Successfully loaded Kernel!", 0x0D, 0x0A, 0
+err_msg:     db "[Stage2 Bootloader] Disk Read Error! Kernel not loaded.", 0x0D, 0x0A, 0
 
-msg: 		db "Stage2: Loading Kernel...", 0x0D, 0x0A, 0
-loaded_msg:	db "Kernel Loaded into Memory!", 0x0D, 0x0A, 0
-err_msg:	db "Disk Read Error! Failed to Load kernel", 0x0D, 0x0A, 0
-
-times 512-($-$$) db 0
+; Pad to 512 bytes
+times 512 - ($ - $$) db 0
